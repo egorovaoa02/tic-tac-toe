908913f721b42871a995d4a7cc6d2826fc8b56d5
PimS12 <120674649+egorovaoa02@users.noreply.github.com>
2023-11-11 23:16:07 +0300

    Add ReadBeanORM

--- src/Model.php
+++ src/Model.php
@@ -4,106 +4,85 @@ declare(strict_types=1);
 
 namespace Egorovaoa02\TicTacToe\Model;
 
-use SQLite3;
+use RedBeanPHP\R;
 
 class Model
 {
-    private $db;
-
     public function __construct($db_path)
     {
-        $this->db = new SQLite3($db_path);
+        R::setup("sqlite:$db_path");
     }
 
     public function createId()
     {
-        $query = "SELECT id FROM result_games ORDER BY id DESC LIMIT 1";
+        $lastGameId = R::getCell("SELECT id FROM results ORDER BY id DESC LIMIT 1");
 
-        $lastGameId = $this->db->query($query);
-
-        if ($row = $lastGameId->fetchArray()) {
-            $lastId = $row[0];
-            return $lastId + 1;
-        } else {
-            return 1;
-        }
+        return $lastGameId ? $lastGameId + 1 : 1;
     }
 
     public function createTables()
     {
+        R::exec("
+            CREATE TABLE IF NOT EXISTS results (
+                id INTEGER PRIMARY KEY,
+                player_name TEXT NOT NULL,
+                player_figure TEXT NOT NULL,
+                field_size TEXT NOT NULL,
+                winning_figure TEXT NOT NULL,
+                created_at  DATETIME NOT NULL
+            )
+        ");
 
-        $query = "CREATE TABLE IF NOT EXISTS result_games (
-            id INTEGER PRIMARY KEY,
-            player_name TEXT NOT NULL,
-            player_figure TEXT NOT NULL,
-            field_size TEXT NOT NULL,
-            winning_figure TEXT NOT NULL,
-            created_at  DATETIME NOT NULL
-        )";
-
-        $this->db->exec($query);
-
-        $query = "CREATE TABLE IF NOT EXISTS tries (
-            id INTEGER PRIMARY KEY,
-            game_id INTEGER NOT NULL,
-            number_try INTEGER NOT NULL,
-            x_coordinate TEXT NOT NULL,
-            o_coordinate TEXT NOT NULL
-        )";
-
-        $this->db->exec($query);
+        R::exec("
+            CREATE TABLE IF NOT EXISTS tries (
+                id INTEGER PRIMARY KEY,
+                game_id INTEGER NOT NULL,
+                number_try INTEGER NOT NULL,
+                x_coordinate TEXT NOT NULL,
+                o_coordinate TEXT NOT NULL
+            )
+        ");
 
         echo "Таблицы успешно созданы";
     }
 
     public function closeConnection()
     {
-        $this->db->close();
+        R::close();
     }
 
     public function storeResult($playerName, $playerFigure, $fieldSize, $winningFigure)
     {
         $now = date('Y-m-d H:i:s');
 
-        $query = "INSERT INTO result_games (player_name, player_figure, field_size, winning_figure, created_at) VALUES ('$playerName', '$playerFigure', '$fieldSize', '$winningFigure', '$now')";
+        $game = R::dispense('results');
+        $game->player_name = $playerName;
+        $game->player_figure = $playerFigure;
+        $game->field_size = $fieldSize;
+        $game->winning_figure = $winningFigure;
+        $game->created_at = $now;
 
-        $this->db->exec($query);
+        R::store($game);
     }
 
     public function storeTry($gameId, $numberTry, $xCoordinate, $oCoordinate)
     {
-        $query = "INSERT INTO tries (game_id, number_try, x_coordinate, o_coordinate) VALUES ('$gameId', '$numberTry', '$xCoordinate', '$oCoordinate')";
+        $try = R::dispense('tries');
+        $try->game_id = $gameId;
+        $try->number_try = $numberTry;
+        $try->x_coordinate = $xCoordinate;
+        $try->o_coordinate = $oCoordinate;
 
-        $this->db->exec($query);
+        R::store($try);
     }
 
     public function getGames()
     {
-        $query = "SELECT * FROM result_games";
-
-        $result = $this->db->query($query);
-
-        $games = [];
-
-        while ($game = $result->fetchArray(SQLITE3_ASSOC)) {
-            $games[] = $game;
-        }
-
-        return $games;
+        return R::findAll('results');
     }
 
     public function getGame($gameId)
     {
-        $query = "SELECT * FROM tries WHERE game_id = '$gameId'";
-
-        $result = $this->db->query($query);
-
-        $tries = [];
-
-        while ($try = $result->fetchArray(SQLITE3_ASSOC)) {
-            $tries[] = $try;
-        }
-
-        return $tries;
+        return R::findAll('tries', 'game_id = ?', [$gameId]);
     }
 }
